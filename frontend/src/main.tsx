e35f499665a59324db927aac4f8ecc9a56e1cd5c
abmta24002tanya-debug <vermatanya932@gmail.com>
2026-08-08 23:40:43 +0530

    updated ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SharedView from "./components/SharedView";
 import "@google/model-viewer";
 import "./styles.css";
 
+const shareToken = new URLSearchParams(location.search).get("share");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {shareToken ? <SharedView token={shareToken} /> : <App />}
   </React.StrictMode>
 );
